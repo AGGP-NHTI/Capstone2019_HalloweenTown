0bc95890069bf0e67c1c57d43c587b2319258f07
Fornan-II <37092360+Fornan-II@users.noreply.github.com>
2018-10-30 14:08:47 -0400

    Simple 3rd Person Char Setup
    Some things are temp, and still need to document code.

---
+++ Assets/Maxen/Scripts/Pawn.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pawn : MonoBehaviour
+{
+    public MoveScript MyMoveScript;
+    public LookScript MyLookScript;
+
+    [HideInInspector]
+    public PlayerController MyController;
+
+    protected virtual void Start ()
+    {
+		
+	}
+	
+	protected virtual void Update ()
+    {
+		
+	}
+
+    public virtual void PassMoveInput(Vector2 value)
+    {
+        if(!MyMoveScript)
+        {
+            Debug.LogWarning(name + " is trying to be passed movement input when it has no MoveScript component assigned!");
+            return;
+        }
+
+        MyMoveScript.MoveHorizontal(value.x);
+        MyMoveScript.MoveVertical(value.y);
+    }
+
+    public virtual void PassLookInput(Vector2 value)
+    {
+        if(!MyLookScript)
+        {
+            Debug.LogWarning(name + " is trying to be passed look input when it has no LookScript component assigned!");
+            return;
+        }
+
+        MyLookScript.MouseInput = value;
+    }
+
+    public virtual void PassFire1(bool value)
+    {
+        if(MyLookScript)
+        {
+            MyLookScript.lockState = value;
+        }
+    }
+
+    public virtual void PassJumpInput(bool value)
+    {
+        if (!MyMoveScript)
+        {
+            Debug.LogWarning(name + " is trying to be passed input when it has no MoveScript component assigned!");
+            return;
+        }
+
+        MyMoveScript.Jump(value);
+    }
+}
---
+++ Assets/Maxen/Scripts/PlayerController.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Pawn ControlledPawn;                 //This is the main Pawn var for this script, use this one.
+    protected Pawn _previouslyControlledPawn;   //This one is used to check and update the ControlChangeEvents. DO NOT use this one.
+    public bool IsControllingPawn { get { return ControlledPawn; } }
+
+
+    protected virtual void Start ()
+    {
+		
+	}
+
+    protected virtual void Update ()
+    {
+        CheckControlChangeEvent();
+
+        if(ControlledPawn)
+        {
+            HandleInput();
+        }
+	}
+
+    #region Control Change Events
+    protected virtual void CheckControlChangeEvent()
+    {
+        if (ControlledPawn && !_previouslyControlledPawn)
+        {
+            OnGainControl();
+            _previouslyControlledPawn = ControlledPawn;
+        }
+        else if (!ControlledPawn && _previouslyControlledPawn)
+        {
+            OnLoseControl();
+            _previouslyControlledPawn = null;
+        }
+    }
+
+    protected virtual void OnGainControl()
+    {
+        ControlledPawn.PassFire1(true);
+    }
+
+    protected virtual void OnLoseControl()
+    {
+        _previouslyControlledPawn.PassFire1(false);
+    }
+    #endregion
+
+    #region Input
+    protected virtual void HandleInput()
+    {
+        if(!ControlledPawn) { return; }
+
+        PassMoveInput(new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")));
+        PassLookInput(new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y")));
+        PassJumpInput(Input.GetButton("Jump"));
+    }
+
+    protected virtual void PassMoveInput(Vector2 value)
+    {
+        if (!ControlledPawn) { return; }
+
+        ControlledPawn.PassMoveInput(value);
+    }
+
+    protected virtual void PassLookInput(Vector2 value)
+    {
+        if (!ControlledPawn) { return; }
+
+        ControlledPawn.PassLookInput(value);
+    }
+
+    protected virtual void PassJumpInput(bool value)
+    {
+        ControlledPawn.PassJumpInput(value);
+    }
+    #endregion
+}
