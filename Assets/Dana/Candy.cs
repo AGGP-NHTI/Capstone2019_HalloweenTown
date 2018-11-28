085f7aed4d0adad65d8913c17c2918366ec4e741
danalemay <37815248+danalemay@users.noreply.github.com>
2018-11-27 20:44:23 -0500

    DoorTrigger

--- Assets/Dana/Candy.cs
+++ Assets/Dana/Candy.cs
@@ -3,23 +3,34 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Candy : MonoBehaviour {
+public class Candy : MonoBehaviour
+{
 
     public int candy = 0;
     public Text testcandy;
-    public GameObject trigger;
     float radius = 3.0f;
-    bool waitforcandy = false;
-	void Start () {
+    public bool waitforcandy = false;
+    bool test = true;
+    void Start()
+    {
         testcandy.text = "Candy: " + candy.ToString();
     }
-	
-	
-	void Update () {
 
-        Collider[] hitColliders = Physics.OverlapSphere(trigger.transform.position, radius);//for testing
-        int i = 0;
-        if (waitforcandy == false)
+
+    void Update()
+    {
+        testcandy.text = "Candy: " + candy.ToString();
+        /*
+        if(waitforcandy)
+        {
+            
+                testcandy.text = "Candy: " + candy.ToString();
+                StartCoroutine(WaitingforCandy());
+                test = false;
+            
+            
+        }
+        /*if (waitforcandy == false)
         {
             while (i < hitColliders.Length)
             {
@@ -34,24 +45,8 @@ public class Candy : MonoBehaviour {
                 }
                 i++;
             }
-        }        
-    }
-
-    IEnumerator WaitingforCandy()
-    {
-        Debug.Log(Time.time);
-        yield return new WaitForSeconds(5);
-        waitforcandy = false;
-        Debug.Log(Time.time);
-        /*Debug.Log("time " + time);
-        if(time > 0)
-        {
-            time--;
-            WaitingforCandy(time);
-        }
-        else
-        {
-            waitforcandy = false;
-        }*/
+        } */
     }
 }
+
+   
