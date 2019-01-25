bb5c361f379b02a5868e83a993a83576824a4c53
danalemay <37815248+danalemay@users.noreply.github.com>
2019-01-25 12:32:23 -0500

    Manager

--- Assets/Dana/Manager.cs
+++ Assets/Dana/Manager.cs
@@ -5,7 +5,11 @@ using UnityEngine;
 public class Manager : MonoBehaviour {
 
     public InputObject io;
+    Pawn pawn;
+    public List<PlayerController> pc;
     public List<GameObject> spawnpts;
+
+    bool startGame = true;
 	void Start () {
         
 		
@@ -13,6 +17,12 @@ public class Manager : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+        if (startGame)
+        {
+            for (int i = 0; i <= pc.Count; i++)
+            {
+                //pc[i].playerInput.
+            }
+        }
 	}
 }
