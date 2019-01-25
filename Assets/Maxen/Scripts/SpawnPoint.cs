3e1459b49c3704a46581a3df55e6fb8f9a7a2358
Fornan-II <37092360+Fornan-II@users.noreply.github.com>
2019-01-25 13:54:35 -0500

    Spawnpoint mvp
    Basic spawnpoint working, just needs to check if players are nearby

---
+++ Assets/Maxen/Scripts/SpawnPoint.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPoint : MonoBehaviour {
+
+    public static List<SpawnPoint> ActiveSpawns = new List<SpawnPoint>();
+
+    protected bool isValid;
+
+    public static SpawnPoint GetRandomSpawn()
+    {
+        if (ActiveSpawns.Count <= 0)
+        {
+            return null;
+        }
+
+        return ActiveSpawns[Random.Range(0, ActiveSpawns.Count)];
+    }
+
+    public static SpawnPoint GetRandomValidSpawn()
+    {
+        return null;
+    }
+
+    protected virtual void Awake()
+    {
+        ActiveSpawns.Add(this);
+    }
+
+    protected virtual void OnDestroy()
+    {
+        ActiveSpawns.Remove(this);
+    }
+
+    /*public virtual bool CheckValidity()
+    {
+        Physics.OverlapSphere(transform.position);
+    }*/
+
+    public virtual void SpawnPlayer(PlayerController pc, GameObject pawnPrefab)
+    {
+        GameObject spawnedGameObject = Instantiate(pawnPrefab, transform.position, transform.rotation);
+        isValid = false;
+
+        Pawn spawnedPawn = spawnedGameObject.GetComponent<Pawn>();
+        if (spawnedPawn)
+        {
+            pc.ControlledPawn = spawnedPawn;
+        }
+        else
+        {
+            Debug.LogWarning(name + " attempted to spawn prefab " + pawnPrefab.name + " that has no Pawn component!");
+        }
+    }
+}
