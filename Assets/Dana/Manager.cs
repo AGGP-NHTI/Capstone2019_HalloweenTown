c9436002260c9058bb32736394740a1e156b7dee
danalemay <37815248+danalemay@users.noreply.github.com>
2019-01-25 13:55:38 -0500

    basic manager

--- Assets/Dana/Manager.cs
+++ Assets/Dana/Manager.cs
@@ -3,26 +3,79 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Manager : MonoBehaviour {
-
-    public InputObject io;
+        
     Pawn pawn;
-    public List<PlayerController> pc;
-    public List<GameObject> spawnpts;
+    //public List<GameObject> spawnpts;
+    public List<InputObject> io;
+    List<bool> joinedGame = new List<bool>();
+    List<bool> readyUp = new List<bool>();
+
+
+    Coroutine timer;
 
     bool startGame = true;
 	void Start () {
+        //sets list of bools to false
         
+        for(int i = 0; i < 4; i++)
+        {
+            joinedGame.Add(false);
+            readyUp.Add(false);
+        }
 		
 	}
 	
-	// Update is called once per frame
 	void Update () {
         if (startGame)
         {
-            for (int i = 0; i <= pc.Count; i++)
-            {
-                //pc[i].playerInput.
+            bool allPlayersReady = true;
+            bool onePlayerInGame = false;
+
+            for (int i = 0; i < io.Count; i++)
+            {         
+                //test if Joined Game                
+                if (io[i].GetStartInput())
+                {
+                    
+                    joinedGame[i] = true;
+                    Debug.Log("Player " + i + " joined the game!");
+                }
+                
+                //test if Ready Up
+                if(io[i].GetJumpInput() && joinedGame[i])
+                {
+                    readyUp[i] = true;
+                    Debug.Log("Player " + i + " is ready!");
+                }
+                
+                
+                if (joinedGame[i])
+                {
+                    onePlayerInGame = true;
+                    if (!readyUp[i])
+                    {
+                        allPlayersReady = false;
+                    }
+                }
+                
             }
+            Debug.Log("oneplayer " + onePlayerInGame.ToString());
+            if(allPlayersReady && onePlayerInGame)
+            {
+                
+                if (timer == null)
+                {
+                    Debug.Log("timer");
+                    timer = StartCoroutine(BeginGameCountDown());
+                }
+            }
+            
         }
 	}
+
+    IEnumerator BeginGameCountDown()
+    {
+        yield return new WaitForSeconds(5);
+        startGame = false;        
+    }
 }
