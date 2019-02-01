68e81767e96f54f3ba60aaa7e15982e91535fa26
danalemay <37815248+danalemay@users.noreply.github.com>
2019-02-01 14:45:18 -0500

    mask ghosty

--- Assets/TeamFolder/Dana/GhostMask.cs
+++ Assets/TeamFolder/Dana/GhostMask.cs
@@ -3,11 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GhostMask : Mask{
-
-    PlayerController pc;
+    
 	
 	void Start () {
-        pc = GetComponentInParent<PlayerController>();
     }
 	
 	
--- Assets/TeamFolder/Dana/Mask.cs
+++ Assets/TeamFolder/Dana/Mask.cs
@@ -8,32 +8,46 @@ public class Mask : MonoBehaviour
     float startHealth;
     bool hasMask = false;
     public bool ultButton = false;
+    public bool help;
     Pawn pawn;
     HealthBar hbar;
-    GhostMask gMask;
+    public GameObject playerPref;
+    public GameObject ghostPref;
+    public GameObject currentModel;
 
     void Start()
     {
         pawn = gameObject.GetComponent<Pawn>();
         hbar = GetComponent<HealthBar>();
-        gMask = gameObject.AddComponent<GhostMask>();//test script
     }
 
 
     void Update()
     {
-
         if (ultButton && hasMask == false)
         {
             Collider[] hitColliders = Physics.OverlapSphere(gameObject.transform.position, 10f);//for testing
-            int i = 0;
-            while (i < hitColliders.Length)
+            
+            for(int i = 0; i < hitColliders.Length; i++)
             {
                 if (hitColliders[i].tag == "Mask")
                 {
                     hasMask = true;
                     UpdateHealth();
+                    gameObject.AddComponent<GhostMask>();//test script
+
                     Destroy(hitColliders[i].gameObject);
+                    Vector3 trans = currentModel.transform.position;
+                    Quaternion rot = currentModel.transform.rotation;
+                    Destroy(currentModel);
+
+                    GameObject mask = Instantiate(ghostPref, gameObject.transform);
+                    mask.transform.position = trans;
+                    mask.transform.rotation = rot;
+                    gameObject.transform.parent = mask.transform;
+                    AlignToMovement al = mask.GetComponent<AlignToMovement>();
+                    al.TrackedRigidBody = gameObject.GetComponent<Rigidbody>();
+                    currentModel = mask;
                 }
             }
         }
@@ -47,7 +61,11 @@ public class Mask : MonoBehaviour
 
         if (hbar.health <= startHealth)
         {
-            Destroy(gMask);
+            Destroy(gameObject.GetComponent<GhostMask>());
+            Destroy(currentModel);
+
+            GameObject mask = Instantiate(playerPref, gameObject.transform);
+            currentModel = mask;
         }
     }
 
