d609ca569e06b7382dbfe767ea8e70a8dd194097
Fornan-II <37092360+Fornan-II@users.noreply.github.com>
2019-02-01 13:12:58 -0500

    InteractionManagement
    Formalizing interaction system to be more usable long-term.

---
+++ Assets/TeamFolder/Maxen/Scripts/Interactions/Interactable.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[System.Serializable]
+public class InteractEvent : UnityEvent<Pawn, Interactable> { }
+
+public class Interactable : MonoBehaviour
+{
+    public InteractEvent OnInteract;
+    public bool AvailableForInteraction = true;
+
+    private void Awake()
+    {
+        if(OnInteract == null)
+        {
+            OnInteract = new InteractEvent();
+        }
+    }
+
+    public virtual void Interact(Pawn source)
+    {
+        OnInteract.Invoke(source, this);
+    }
+}
---
+++ Assets/TeamFolder/Maxen/Scripts/Interactions/InteractionManager.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractionManager : MonoBehaviour
+{
+    public Pawn MyPawn;
+    public Interactable selectedInteractable;
+
+    public float interactionRange = 3.0f;
+
+    public virtual void TryToInteract()
+    {
+        if (selectedInteractable != null)
+        {
+            selectedInteractable.Interact(MyPawn);
+        }
+    }
+
+    protected virtual void FixedUpdate()
+    {
+        FindInteractableToSelect();
+    }
+
+    protected virtual void FindInteractableToSelect()
+    {
+        selectedInteractable = null;
+        Collider[] nearbyColliders = Physics.OverlapSphere(transform.position, interactionRange);
+
+        List<Interactable> nearbyInteractables = new List<Interactable>();
+        foreach(Collider c in nearbyColliders)
+        {
+            Interactable i = c.GetComponent<Interactable>();
+            nearbyInteractables.Add(i);
+        }
+
+        //Just using the nearest interactable atm
+        float nearestInteractableSqrDistance = -1.0f;
+        foreach(Interactable i in nearbyInteractables)
+        {
+            if(i.AvailableForInteraction)
+            {
+                float sqrDistance = (i.transform.position - transform.position).sqrMagnitude;
+
+                if(nearestInteractableSqrDistance < 0.0f)
+                {
+                    nearestInteractableSqrDistance = sqrDistance;
+                    selectedInteractable = i;
+                }
+                else if(nearestInteractableSqrDistance > sqrDistance)
+                {
+                    nearestInteractableSqrDistance = sqrDistance;
+                    selectedInteractable = i;
+                }
+            }
+        }
+    }
+}
