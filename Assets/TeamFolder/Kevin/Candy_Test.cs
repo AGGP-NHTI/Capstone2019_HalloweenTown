0ca141ccf9062d0141d5e29c610efe989469d477
Kevmonk2000 <37087819+Kevmonk2000@users.noreply.github.com>
2019-02-19 16:02:30 -0500

    Door Particle

---
+++ Assets/TeamFolder/Kevin/Candy_Test.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Candy_Test : MonoBehaviour {
+
+    public GameObject dropCandy;
+
+    public Transform player;
+
+    public int numCandy = 3;
+
+
+    // Use this for initialization
+    void Start () {
+
+        
+
+        for (int i = 0; i < numCandy; i++)
+        {
+            Instantiate(dropCandy, player.transform.position, Quaternion.identity);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
