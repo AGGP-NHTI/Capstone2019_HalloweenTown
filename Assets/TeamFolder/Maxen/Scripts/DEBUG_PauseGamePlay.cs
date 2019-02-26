b6f0cc500ee8790dfad6ebf2f136ffca9bd5d643
Fornan-II <37092360+Fornan-II@users.noreply.github.com>
2019-02-26 13:37:25 -0500

    More Keyboard/Mouse Support

---
+++ Assets/TeamFolder/Maxen/Scripts/DEBUG_PauseGamePlay.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DEBUG_PauseGamePlay : MonoBehaviour
+{
+    public KeyCode togglePauseKey = KeyCode.P;
+
+    protected bool isPaused = false;
+    protected float runtimeScale;
+
+	void Start ()
+    {
+        runtimeScale = Time.timeScale;
+	}
+	
+	
+	void Update ()
+    {
+		if(Input.GetKeyDown(togglePauseKey))
+        {
+            UpdatePausedState(!isPaused);
+        }
+	}
+
+    void UpdatePausedState(bool newState)
+    {
+        if(newState && !isPaused)
+        {
+            runtimeScale = Time.timeScale;
+
+            Time.timeScale = 0.0f;
+        }
+        else if(isPaused && !newState)
+        {
+            Time.timeScale = runtimeScale;
+        }
+
+        isPaused = newState;
+    }
+}
