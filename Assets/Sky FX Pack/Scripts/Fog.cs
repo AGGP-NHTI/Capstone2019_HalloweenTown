6b963d179a2525de56a55c72042471682c8cead6
gr8y <37554278+gr8y@users.noreply.github.com>
2019-02-26 15:13:43 -0500

    Added fog

---
+++ Assets/Sky FX Pack/Scripts/Fog.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fog : MonoBehaviour {
+
+    public Renderer rend;
+    public float scrollSpeed = 0.5f;
+
+    // Use this for initialization
+    void Start () {
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        float offset = Time.time * scrollSpeed;
+        rend.material.SetTextureOffset("_MainTex", new Vector2(offset, 0));
+    }
+}
