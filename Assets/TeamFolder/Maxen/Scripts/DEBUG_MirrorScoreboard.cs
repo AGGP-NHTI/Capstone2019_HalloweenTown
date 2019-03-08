2951957ec2ed2a4b4f53e1c5551e91f9ecea09b2
Fornan-II <37092360+Fornan-II@users.noreply.github.com>
2019-03-08 15:01:57 -0500

    Proper Candy Adding
    Will poke more soon

---
+++ Assets/TeamFolder/Maxen/Scripts/DEBUG_MirrorScoreboard.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DEBUG_MirrorScoreboard : MonoBehaviour
+{
+    public List<int> PlayerScores;
+
+    private void Start()
+    {
+        PlayerScores = new List<int>();
+    }
+
+    private void Update()
+    {
+        PlayerScores.Clear();
+        if(Candy.Scoreboard != null)
+        {
+            foreach(int score in Candy.Scoreboard.Values)
+            {
+                PlayerScores.Add(score);
+            }
+        }
+    }
+}
