7b1fccbd080373a12a6611853fd2e424068795d8
Fornan-II <37092360+Fornan-II@users.noreply.github.com>
2019-03-11 21:30:32 -0400

    Console so we can issue commands/do debug cmds in builds

---
+++ Assets/TeamFolder/Maxen/Scripts/BuildUtil/BuildConsole.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BuildConsole : MonoBehaviour
+{
+#region Static Stuff
+    const string outputPrefix = "> ";
+    public static void WriteLine(object msg)
+    {
+        if(_instance)
+        {
+            _instance.output.text += outputPrefix + msg.ToString() + "\n";
+        }
+    }
+
+    protected static BuildConsole _instance;
+#endregion
+
+    protected void Awake()
+    {
+        if(_instance)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            _instance = this;
+        }
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public KeyCode ConsoleVisibleToggleKey = KeyCode.BackQuote;
+    public GameObject console;
+    public Text output;
+    public InputField input;
+
+    protected void Update()
+    {
+        if (Input.GetKeyDown(ConsoleVisibleToggleKey))
+        {
+            console.SetActive(!console.activeSelf);
+            if(console.activeSelf)
+            {
+                input.Select();
+                input.ActivateInputField();
+            }
+        }
+    }
+}
---
+++ Assets/TeamFolder/Maxen/Scripts/BuildUtil/ConsoleCommandParser.cs
@@ -0,0 +1,118 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ConsoleCommandParser : MonoBehaviour
+{
+    UnityEngine.UI.InputField _field;
+    private void Awake()
+    {
+        _field = GetComponent<UnityEngine.UI.InputField>();
+    }
+
+    private void Update()
+    {
+        if(gameObject.activeSelf && _field)
+        {
+            if(!_field.IsActive())
+            {
+                SetFocus();
+            }
+        }
+    }
+
+    //Main Parser
+    public void ParseCommand(string cmd)
+    {
+        cmd = cmd.ToLower();
+        string[] splitCmd = cmd.Split(' ');
+
+        switch(splitCmd[0])
+        {
+            case "round":
+                {
+                    round(splitCmd);
+                    break;
+                }
+            default:
+                {
+                    BuildConsole.WriteLine("Invalid command");
+                    break;
+                }
+        }
+
+        _field.text = "";
+        SetFocus();
+    }
+
+    //Command Specific functions
+    [Header("For command \"round\"")]
+    public GameObject RoundManagerPrefab;
+    protected void round(string[] keywords)
+    {
+        RoundManager activeRM = FindObjectOfType<RoundManager>();
+        if(keywords.Length < 2)
+        {
+            BuildConsole.WriteLine("Insufficient parameters");
+            return;
+        }
+
+        switch(keywords[1])
+        {
+            case "time":
+                {
+                    if (keywords[2] == "get")
+                    {
+                        if(activeRM)
+                        {
+                            BuildConsole.WriteLine(activeRM.roundElapsedTime);
+                        }
+                        else
+                        {
+                            BuildConsole.WriteLine("Round not active");
+                        }
+                        break;
+                    }
+                    else
+                    {
+                        float value;
+                        if(float.TryParse(keywords[2], out value))
+                        {
+                            if (activeRM)
+                            {
+                                BuildConsole.WriteLine(activeRM.roundElapsedTime);
+                            }
+                            else if(RoundManagerPrefab)
+                            {
+                                RoundManager rm = RoundManagerPrefab.GetComponent<RoundManager>();
+                                if(rm)
+                                {
+                                    rm.roundRunningEndTime = value;
+                                    BuildConsole.WriteLine("Round time set to" + value);
+                                }
+                            }
+                        }
+                        else
+                        {
+                            BuildConsole.WriteLine("Expecting \'get\' or number as parameters, found neither");
+                        }
+                    }
+                    break;
+                }
+            default:
+                {
+                    BuildConsole.WriteLine("Invalid parameters for round");
+                    break;
+                }
+        }
+    }
+
+    protected void SetFocus()
+    {
+        _field.Select();
+        _field.ActivateInputField();
+        //EventSystem.current.SetSelectedGameObject(gameObject, null);
+        //_field.OnPointerClick(new PointerEventData(EventSystem.current));
+    }
+}
