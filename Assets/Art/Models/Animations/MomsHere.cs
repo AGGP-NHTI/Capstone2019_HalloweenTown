a303305f46acb3136f6cefe53e6b59464c195ef3
gr8y <37554278+gr8y@users.noreply.github.com>
2019-04-19 15:08:33 -0400

    Added mom cutscene

---
+++ Assets/Art/Models/Animations/MomsHere.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MomsHere : MonoBehaviour {
+
+    public Animation momVan;
+    public Animation gateOpen;
+
+    // Use this for initialization
+    void Start () {
+        momVan.Play();
+        gateOpen.Play();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
