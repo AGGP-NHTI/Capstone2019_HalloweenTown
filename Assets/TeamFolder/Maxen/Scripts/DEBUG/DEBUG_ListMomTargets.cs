3418d48e40608f9fbe4dd94fb64996720aa024be
Fornan-II <37092360+Fornan-II@users.noreply.github.com>
2019-05-07 10:03:10 -0400

    Mom networking debug info

---
+++ Assets/TeamFolder/Maxen/Scripts/DEBUG/DEBUG_ListMomTargets.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DEBUG_ListMomTargets : MonoBehaviour
+{
+    public MomPawn mom;
+    public Text output;
+	
+	void Update ()
+    {
+		if(mom)
+        {
+            string msg = "";
+            for(int t = 0; t < 4; t++)
+            {
+                string targetName = "NULL";
+                if(t < mom.Targets.Count)
+                {
+                    if(mom.Targets[t])
+                    {
+                        targetName = "Player " + mom.Targets[t].name.ToString();
+                    }
+                }
+
+                msg += targetName + "\n";
+            }
+
+            output.text = msg;
+        }
+	}
+}
